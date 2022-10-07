d14f0c04fca96b98bf837a57901af9e75cc356a8
Mimieam <t.echismimi@gmail.com>
2022-10-07 04:07:10 -0500

    updated to a chrome ext part 2

---
+++ src/popup/_Folder.svelte
@@ -0,0 +1,116 @@
+<script>
+  import Website from '../Website.svelte';
+  import { fly, slide }  from 'svelte/transition'
+
+  export let expanded = false;
+  export let name=null;
+  export let files=null;
+  export let ondrop=null;
+  export let ondragstart=null;
+  export let ondragover=null;
+  export let depth=0;
+
+
+
+
+  function toggle() {
+      expanded = !expanded;
+  }
+  $: arrowDown = expanded
+
+  function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  console.log("dragging...", ev.target)
+  ev.currentTarget.style.border = "dashed";
+  ev.dataTransfer.setData("target_id", [ev.target.id]);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  console.log("droppping. @..", ev.target)
+  var data = ev.dataTransfer.getData("target_id");
+
+  console.log("drop=", data)
+  console.log("closest UL =", ev.target.closest("ul:not(div)"))
+  const _ul = ev.target.closest("ul")
+  if (_ul){
+    _ul.appendChild(document.getElementById(data))
+  } else {
+    document.appendChild(data)
+  }
+}
+
+</script>
+
+
+<span class:expanded on:click={toggle}>
+  <span class="arrow" class:arrowDown>&#x25b6</span>
+  {name}
+</span>
+
+<!-- draggable="true" ondragstart="drag(event)"  -->
+
+{#if expanded}
+    <ul transition:slide="{{duration:500}}" on:drop={drop} on:dragover={allowDrop} id={"ul"+window.crypto.randomUUID()}>
+        {#each files as file}
+            <li draggable="true" on:dragstart={drag} id={"li"+ window.crypto.randomUUID()}>
+                {#if file.files}
+                    <svelte:self {...file} name={`${file.name} (Level-${depth})`} depth={depth+1}/>
+                {:else}
+                    <Website {...file} name={`${file.name} (Level-${depth})`}/>
+                {/if}
+            </li>
+        {/each}
+    </ul>
+{/if}
+
+<style>
+    span {
+        padding: 0 0 0 0.11em;
+        background: url(/assets/icons/folder.svg) 0 0.1em no-repeat;
+        background-size: 1em 1em;
+        font-weight: bold;
+        cursor: pointer;
+    }
+
+    .expanded {
+        background-image: url(/assets/icons/folder-open.svg);
+    }
+
+    ul {
+        padding: 0.2em 0 0 0.5em;
+        margin: 0 0 0 0.5em;
+        list-style: none;
+        border-left: 1px solid #eee;
+    }
+
+    li {
+        margin: 0.2em 0;
+    }
+
+  /* ul {
+		margin: 0;
+		list-style: none;
+		padding-left: 1.2rem;
+		user-select: none;
+	} */
+	.no-arrow { padding-left: 1.0rem; }
+	.arrow {
+		cursor: pointer;
+		display: inline-block;
+		/* transition: transform 200ms; */
+	}
+	.arrowDown { transform: rotate(90deg); }
+  ul {
+    /* background: #1a202c; */
+    opacity: .9;
+    color:#a0aec0
+  }
+  li {
+    /* background: #2d3748; */
+    color:#a0aec0
+  }
+</style>
---
+++ src/popup/nodes.js
@@ -0,0 +1,164 @@
+let root = [
+  {
+      name: 'News',
+      children: [
+          { name: 'https://cnn.com' },
+          { name: 'https://msnbc.com' },
+          { name: 'https://DW.com' }
+      ]
+  },
+  {
+      name: 'Folder 2',
+      children: [
+          {
+              name: 'Folder 2.1',
+              children: [
+                  { name: 'https://google.com' },
+                  { name: 'https://yahoo.com' },
+                  { name: 'https://bing.com' },
+                  { name: 'https://msn.com' },
+              ]
+          },
+          {
+              name: 'Folder 2.2',
+              children: [
+                  { name: 'apple.com' },
+                  { name: 'amazon.com' }
+              ]
+          },
+          { name: 'zillow.com' },
+          { name: 'alsjdf.com' },
+          { name: 'somerandomwebsite.com' }
+      ]
+  },
+  { name: 'tabsorter2.com' }
+];
+
+
+
+export class Racine {
+  constructor(root, id_prefix='node'){
+    this.root = root
+    this.id_prefix = id_prefix
+    this.total_nodes = 0
+    this.nodes = {
+      [`${ this.id_prefix }0`]: {
+        id: `${ this.id_prefix }0`,
+        name: 'root',
+        children: []
+      },
+    }
+    this.mapping = {}
+  }
+  // dfs
+  build = (root, parent_id = 0) => {
+    // ok ok... this will need to be revised in a future version
+    return root.map(n => {
+      this.total_nodes = this.total_nodes + 1
+      const res = {
+        [`${this.id_prefix}${ this.total_nodes }`]: {
+          id:`${this.id_prefix}${this.total_nodes }`,
+          parent_id: `${this.id_prefix}${parent_id }`,
+          children: (n && n.children) ? this.build(n.children, this.total_nodes) : [],
+          name: n.name,
+        }
+      }
+
+      this.nodes = { ...this.nodes, ...res }
+      let full_parent_id = `${ this.id_prefix }${ parent_id }`
+
+      if (this.nodes[full_parent_id] && this.nodes[full_parent_id].children) {
+        this.nodes[full_parent_id].children = [...this.nodes[full_parent_id].children, ...Object.values(res)]
+        console.log(Object.values(this.nodes[full_parent_id].children))
+        console.log(Object.values(res))
+      }
+      return Object.values(res)
+    }).flatMap(x=>x) // fix children being nested arrays of single values... ugh
+  }
+  // bfs  .. sorta
+  undo = (nodes) => {
+    const breadthFirstSearch = (root, output = []) => {
+      // Handle the edge case that the function is
+      // called without providing the root node
+      if (!root) return output;
+
+      // We will use a queue to store each node in
+      // breadth-first order, initializing it with
+      // the root node
+      const q = new Queue();
+      q.enqueue(root);
+
+      // While the queue is not empty, we have more
+      // nodes to "process"
+      while (!q.isEmpty()) {
+        // Retrieve the oldest node from the queue
+        const node = q.dequeue();
+
+        // "Process" the node
+        output.push(node.val);
+
+        // Enqueue all of the current node's children
+        // If the node is a leaf, this loop will not
+        // execute
+        for (let child of node.children) {
+          q.enqueue(child);
+        }
+      }
+
+      // Once the queue is empty, we can be certain
+      // that we have visited every node in the tree
+      return output;
+    };
+  }
+
+  move = (sourceNodeID, destinationNodeID, idx=0) => {
+    let _parent = this.nodes[this.nodes[sourceNodeID].parent_id]
+    if (_parent) {
+      //update old parent
+      _parent.children = _parent.children.filter(c => c.id != sourceNodeID)
+      this.nodes[sourceNodeID].parent_id = destinationNodeID
+      // update new parent
+      let new_parent = this.nodes[destinationNodeID]
+      new_parent.children = new_parent.children.filter(c => c.id != sourceNodeID)
+      new_parent.children.splice(idx, 0, this.nodes[sourceNodeID])
+    }
+    return this.nodes
+  }
+
+  get_root = (name_only = true) => {
+    let root_name = `${this.id_prefix}0`
+    return name_only? root_name: this.nodes[root_name]
+  }
+
+  get_children = (node) => {
+    return Object.values(this.nodes).filter(o => o?.parent_id == node).map(o => o.id)
+  }
+
+  save = () => {
+    return JSON.stringify(this.root)
+  }
+
+  get_depth(item, iteration = 0) {
+    if (!item.children.length) return iteration;
+    iteration++;
+    return Math.max(...item.children.map(i => this.get_depth(i, iteration)));
+  }
+
+  print = (n) => {
+    let children = this.get_children(n)
+    if (children.length) {
+      console.log(n.padEnd(6), `|[${ children }]`)
+      for (const c of children) {
+        this.print(c)
+      }
+    }
+  }
+}
+
+
+// r = new Racine(root)
+// r.build(root)
+// // r.nodes
+// // r.move('node11', 'node0')
+// // r.move('node2', 'node10')
+// r.print("node0")
